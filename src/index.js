5653775f04b861e6f4993aaeb2f802aa94fbc247
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-03-05 19:31:56 +0000

    UI cleanup and apollo connection to db configured

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// this is essential for connecting our front and back
+const client = new ApolloClient({
+    uri: 'http://localhost:4444/graphql'
+});
+
+ReactDOM.render(<ApolloProvider client={client}><App/></ApolloProvider>, document.getElementById('root'));
+
