1773c60ebd1987317e3c4b932933e145e68aeb3f
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-03-09 20:49:08 +0000

    a

--- src/index.js
+++ src/index.js
@@ -8,23 +8,54 @@ import Signin from "./components/auth/Signin";
 
 import ApolloClient from "apollo-boost";
 import {ApolloProvider} from "react-apollo";
+import withSession from "./components/shared/withSession";
+import Navbar from "./components/shared/Navbar";
+import Search from "./components/Cases/Search";
+import Profile from "./components/Profile/Profile";
 
 // this is essential for connecting our front and back
 const client = new ApolloClient({
-    uri: 'http://localhost:4444/graphql'
+    uri: 'http://localhost:4444/graphql',
+    // this is for user auth
+    fetchOptions: {
+        credentials: 'include' // this allows to send the token from the local storage to the backend
+    },
+    request: operation => {
+        const token = localStorage.getItem('token');
+        operation.setContext({
+            headers: {
+                authorization: token
+            }
+        });
+    },
+    onError: ({networkError}) => {
+        if (networkError) {
+            console.log('Network error: ', networkError);
+            if (networkError.statusCode === 401) {
+                localStorage.setItem('token', '')
+            }
+        }
+    }
 });
 
-const Root = () => (
+const Root = ({refetch, session}) => (
     <Router>
-        <Switch>
-            <Route path="/" exact component={App} />
-            <Route path="/signin" component={Signin} />
-            <Route path="/signup" component={Signup} />
-            <Redirect to="/"/>
-        </Switch>
+        <React.Fragment>
+            <Navbar session={session}/>
+            <Switch>
+                <Route path="/" exact component={App} />
+                <Route path="/search" exact component={Search} />
+                <Route path='/profile' component={Profile}/>
+                <Route path="/signin" render={() => <Signin refetch={refetch}/>} />
+                <Route path="/signup" render={() => <Signup refetch={refetch}/>} />
+                <Redirect to="/"/>
+            </Switch>
+        </React.Fragment>
     </Router>
 );
 
+const RootWithSession = withSession(Root);
+
 // with the ApolloProvider wrapped around App. We can perform any query/mutation in any of our components
-ReactDOM.render(<ApolloProvider client={client}><Root/></ApolloProvider>, document.getElementById('root'));
+ReactDOM.render(<ApolloProvider client={client}><RootWithSession/></ApolloProvider>, document.getElementById('root'));
 
