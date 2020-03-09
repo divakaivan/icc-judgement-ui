547863df2438c6bd79d449f4999e6f793468eb89
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-03-09 22:24:06 +0000

    home page css improvements

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
-
+import {Button} from "react-bootstrap";
 import {Query} from "react-apollo";
 import {getAllCases} from "../queries/queries";
 import LoadingSpinner from "./shared/LoadingSpinner";
 
 
-const App = () => {
+const App = ({session}) => {
+
     return (
-        <div className="App">
+        <div className="align-content-center text-center container-fluid">
+            <h4 className="neon">Welcome, {session && session.getCurrentUser ? session.getCurrentUser.summonerName : "summoner"}! <br/>To the <br/>Fields of Justice</h4>
+            <h6 className="neon-right">
+                Wanna remove toxic players? <br/>
+                {session && session.getCurrentUser ?
+                    <Button variant="danger" href='/cases'>Cases</Button> :
+                    <Button variant="danger" href='/signup'>Sign up</Button>
+                }
+            </h6>
             <h1>Home</h1>
             <Query query={getAllCases}>
                 {({data, loading, error}) => {
